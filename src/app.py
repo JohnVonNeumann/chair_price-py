709da403deb8d2204422583ad26637ab7eacd2be
JohnVonNeumann <louiswillcock@gmail.com>
2017-03-09 22:06:59 +1100

    edited the app.py file and imported some new libraries into our venv, first exposure to beautiful soup, a screenscraping library that helps with pulling data from websites with some help beforehand of course. so you set the bs up on vars and that, then input what elements you wish to watch and it can return the plaintext for you. pretty tidy little thing to be honest. id always wondered how it worked. i might give it a crack on some other sites like hotcopper, see if i can pull stock prices and other such things.

--- src/app.py
+++ src/app.py
@@ -1,8 +1,12 @@
 __author__ = 'lw'
 
 import requests
+from bs4 import BeautifulSoup
 
-request = requests.get("http://www.google.com")
+request = requests.get("http://www.johnlewis.com/house-by-john-lewis-hinton-office-chair/p2083183?colour=Red")
+content = request.content
+soup = BeautifulSoup(content, "html.parser")
+element = soup.find("span", {"class": "now-price", "itemprop": "price"})
+print(element.text)
 
-print(request.content)
-
+# <span class="now-price" itemprop="price"> £115.00 </span>
