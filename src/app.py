b3f11c5c464699a2377921cecbfc6fa020498b5d
JohnVonNeumann <louiswillcock@gmail.com>
2017-03-09 23:58:42 +1100

    finalising this repo, pretty simple intro to pycharm and venv's, moving onto proper web app with apis now.

--- src/app.py
+++ src/app.py
@@ -7,6 +7,15 @@ request = requests.get("http://www.johnlewis.com/house-by-john-lewis-hinton-offi
 content = request.content
 soup = BeautifulSoup(content, "html.parser")
 element = soup.find("span", {"class": "now-price", "itemprop": "price"})
-print(element.text)
+string_price = element.text.strip()
 
+price_without_symbol = string_price[1:]
+
+price = float(price_without_symbol)
+
+if price < 200:
+    print("Buy the chair")
+    print("The current price is {}.".format(price))
+else:
+    print("Don't buy the chair")
 # <span class="now-price" itemprop="price"> £115.00 </span>
